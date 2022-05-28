557bee620bd8aed84c756a3eaab16ebdaaeca97f
Chu-chuu <105371867+Chu-chuu@users.noreply.github.com>
2022-05-28 20:51:32 +0100

    Add files via upload

---
+++ Classes-and-Objects/main.py
@@ -0,0 +1,31 @@
+from unicodedata import name
+
+
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        # Instance Variable
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+        pass
+
+    def change_name(self):
+        print("Peter")
+
+    def change_age(self):
+        print("34")
+
+    def add_track(self):
+        print("UI/UX")   
+
+    def get_score(self):
+        print("20.90") 
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+Bob.change_name()
+Bob.change_age()
+Bob.add_track()
+Bob.get_score()
